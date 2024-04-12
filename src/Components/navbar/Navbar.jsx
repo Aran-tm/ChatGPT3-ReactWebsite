655a1fbc5207764913cb50982fb2de04edbe31a9
Aran-tm <armandortmontes@gmail.com>
2024-04-12 15:13:16 -0500

    adding other styles

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import './navbar.css'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import './navbar.css';
+import logo  from '../../assets/logo.svg';
+
+// BEM -> Block Element Modifier
 
 function Navbar() {
   return (
-    <div>
-      Navbar
+    <div className='gpt3__navbar'>
+          <div className='gpt3__navbar-links'>
+              <div className='gpt3__navbar-links_logo'>
+                  <img src={logo} alt="logo" />
+              </div>
+
+              {/** This are my main navbar links */}
+              <div className='gpt3__navbar-links_container'>
+                  <p><a href="#home">Home</a></p>
+                  <p><a href="#wgpt3">What is GPT3 ?</a></p>
+                  <p><a href="#possibility">Open AI</a></p>
+                  <p><a href="#features">Case Studies</a></p>
+                  <p><a href="#blog">Library</a></p>
+              </div>
+      </div>
     </div>
   )
 }
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import './header.css'
 function Header() {
   return (
     <div>
-      
+      Header
     </div>
   )
 }
