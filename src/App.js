7933c1614d2d9c3ef08702c457b242690eeec19e
Aran-tm <armandortmontes@gmail.com>
2024-04-12 13:15:42 -0500

    putting all my components into my App.js Main Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React from 'react'
 
 // importing all my components
-import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Brand, CTA,  Navbar } from './components';
 import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 
 // this is my first React component
 const App = () => {
 
   return (
-    <div>
-        <h1>GPT-3</h1>
+      <div className='App'>
+        <div className='gradient__bg'>
+            {/** This are my Navbar and Header components */}
+            <Navbar />
+            <Header />
+        </div>
+        
+        {/** This are my container components */}
+        <Brand />
+        <WhatGPT3 />
+        <Features />
+        <Possibility />
+        <CTA />
+        <Blog />
+        <Footer />
     </div>
   )
 }
