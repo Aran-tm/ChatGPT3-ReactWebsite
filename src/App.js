7d8907cb6f910504803329e034b8a847b61e79de
Aran-tm <armandortmontes@gmail.com>
2024-04-12 12:49:22 -0500

    importing and exportin all my container components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 // importing all my components
 import { Article, Brand, CTA, Feature, Navbar } from './components';
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
 
 // this is my first React component
 const App = () => {
