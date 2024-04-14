eacdbf411a6520ca711b3c5bf204ee2b65c5b623
Aran-tm <armandortmontes@gmail.com>
2024-04-14 00:12:06 -0500

    adding big size

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,7 +7,7 @@ import ai from '../../assets/ai.png'
 function Header() {
   return (
     <div className='gpt__header section__padding' id='home'>
-        <div className='gpt__header-content'>
+        <div className='gpt3__header-content'>
             <h1 className='gradient__text'>
                 Let's Build Something amazing with GPT-3 OpenAI
               </h1>
