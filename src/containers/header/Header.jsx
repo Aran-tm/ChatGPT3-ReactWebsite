eb7f68bf1b4ee090335023c50bd05ae5138fb5b8
Aran-tm <armandortmontes@gmail.com>
2024-04-14 19:40:14 -0500

    adding new styles

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -6,28 +6,26 @@ import ai from '../../assets/ai.png'
 
 function Header() {
   return (
-    <div className='gpt__header section__padding' id='home'>
-        <div className='gpt3__header-content'>
-            <h1 className='gradient__text'>
-                Let's Build Something amazing with GPT-3 OpenAI
-              </h1>
-              <p>ChatGPT is an artificial intelligence (AI) chatbot that leverages the Generative Pre-trained Transformer (GPT) model to engage in human-like conversations. It's designed to interact with users in a conversational manner, optimized for dialogue by human trainers.</p>
-            
-            <div className='gpt3__header-content__input'>
-                <input type="email" placeholder='Your Email Adress' />
-                <button type='button'>Get Started</button>
-            </div>
-            
-            <div className='gpt3__header-content__people'>
-                  <img src={people} alt="people" />
-                  <p>1600 people requested access a visit in last 24 hours</p>
-            </div>
-            
-            <div className='gpt3__header-image'>
-                <img src={ai} alt="ai" />
-            </div>
-        </div>
+    <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+      <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+
+      <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Your Email Address" />
+        <button type="button">Get Started</button>
+      </div>
+
+      <div className="gpt3__header-content__people">
+        <img src={people} />
+        <p>1,600 people requested access a visit in last 24 hours</p>
+      </div>
     </div>
+
+    <div className="gpt3__header-image">
+      <img src={ai} />
+    </div>
+  </div>
   )
 }
 
