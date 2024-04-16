df76fd7e207d0767ae5bd5dee77be8e57f2617da
Aran-tm <armandortmontes@gmail.com>
2024-04-16 13:30:47 -0500

    adding just a width for brands dimensions

--- src/Components/brand/Brand.jsx
+++ src/Components/brand/Brand.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './brand.css'
 
+import { google, slack, atlassian, dropbox, shopify } from './imports'
+
 function Brand() {
   return (
-    <div>
-      
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="google" />
+      </div>
+      <div>
+        <img src={atlassian} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="google" />
+      </div>
     </div>
   )
 }
